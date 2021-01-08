f1099e62c2ecb9e422aa97beeb60e9b8666a2902
Abdullah Kareem <67718280+abdullahkareem@users.noreply.github.com>
2021-01-08 03:28:22 +0300

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,8 @@
+$ports=0..500;
+$ip="192.168.0.1";
+
+foreach ($port in $ports) {try{$socket=new-object system.net.sockets.tcpclient($ip,$port);}
+
+catch{};
+
+if ($socket -eq $null) {echo $ip":"$port" - Closed";} else{echo $ip":"$port" - Open"; $socket=$null;}}
